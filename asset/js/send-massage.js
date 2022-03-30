535329bd419e59a215ef8ddc2457c9ee3066a43d
suryajayantara <ptsuryajayantara01@gmail.com>
2022-03-30 10:37:58 +0800

    :smile: image added

--- asset/js/send-massage.js
+++ asset/js/send-massage.js
@@ -5,5 +5,5 @@ var sendMassage = () => {
 
   var massage = `Halo Pratama Dan Vera ! %0ASelamat ya atas pernikahannya. Semoga ${wish}.Nsgomong-ngomong aku *_${attandace}_* ke acara mu  ._ %0A%0A Salam hangat %0A *_${name}_* "`;
 
-  location.href = `https://api.whatsapp.com/send?phone=6281237500352&text=${massage}`;
+  location.href = `https://api.whatsapp.com/send?phone=6281933049323&text=${massage}`;
 };
