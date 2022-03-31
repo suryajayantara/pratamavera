6e99cc2f3510389c27a693433e732d7171cf26d3
suryajayantara <ptsuryajayantara01@gmail.com>
2022-03-31 14:04:06 +0800

    :smile: minified everything

--- asset/js/music.js
+++ asset/js/music.js
@@ -1,6 +1,21 @@
 var musicEmbed = document.querySelector("#musicPlay");
 musicEmbed.play();
 
+if (musicEmbed !== undefined) {
+  musicEmbed
+    .then(() => {
+      // Start whatever you need to do only after playback
+      // has begun.
+    })
+    .catch((error) => {
+      if (error.name === "NotAllowedError") {
+        showPlayButton(musicEmbed);
+      } else {
+        // Handle a load or playback error
+      }
+    });
+}
+
 
 var musicControl = () => {
   var music = document.querySelector("#musicPlay");
